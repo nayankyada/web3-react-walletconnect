8123b61d278160db44dfbcdd568af08b91050e0d
nayankyada <kayadanayan@gmail.com>
2022-04-19 16:51:24 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./style/index.css";
+
+function App() {
+  return (
+    <div className="App test">
+      <header>
+        <h1 className="font-extrabold text-2xl w-full flex justify-center h-full items-center text-primary-1">
+          Welcome to Create-React-App, Apollo-Graphql, Tailwind-CSS scaffold project
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
